e9e1f07fbfbfc4bb9fbd383d036ba98989b89fda
Samidh Desai <samidhdesai@gmail.com>
2019-05-01 15:26:32 -0500

    Adds final details to About page

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -1,9 +1,44 @@
-//Modal 1
 var modal = document.getElementById('myModal');
 var img = document.getElementById('myImg');
+var img2 = document.getElementById('myImg2');
+var img3 = document.getElementById('myImg3');
+var img4 = document.getElementById('myImg4');
+var img5 = document.getElementById('myImg5');
+var img6 = document.getElementById('myImg6');
 var modalImg = document.getElementById("img01");
 
+
 img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+img2.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+img3.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+img4.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+img5.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+img6.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
   captionText.innerHTML = this.alt;
@@ -13,21 +48,4 @@ var span = document.getElementsByClassName("close")[0];
 
 span.onclick = function() { 
   modal.style.display = "none";
-}
-
-//Modal 2
-var modal2 = document.getElementById('myModal2');
-var img2 = document.getElementById('myImg2');
-var modalImg2 = document.getElementById("img02");
-
-img2.onclick = function(){
-  modal2.style.display = "block";
-  modalImg2.src = this.src;
-  captionText.innerHTML = this.alt;
-}
-
-var span2 = document.getElementsByClassName("close2")[0];
-
-span2.onclick = function() { 
-  modal2.style.display = "none";
 }
